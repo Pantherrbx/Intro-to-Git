0279798e00fe11cb250587c2ac9b2762ffde3132
Shivam Kumar <shiviam02@gmail.com>
2025-11-07 14:52:25 +0530

    Updated code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Shivam")
+# Simple Calculator
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+
+if op == '+':
+    print("Result =", a + b)
+elif op == '-':
+    print("Result =", a - b)
+elif op == '*':
+    print("Result =", a * b)
+elif op == '/':
+    if b == 0:
+        print("Cannot divide by zero")
+    else:
+        print("Result =", a / b)
+else:
+    print("Invalid operator")
